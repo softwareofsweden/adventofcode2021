7108fc4c4ac30c0c5a9ed844fcc99bcc89dde8e1
Martin Sandberg <martin.sandberg79@gmail.com>
2021-12-05 16:25:32 +0100

    Better console output with duration etc.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,20 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Day01.SolveP1());
-            Console.WriteLine(Day01.SolveP2());
-            Console.WriteLine(Day02.SolveP1());
-            Console.WriteLine(Day02.SolveP2());
-            Console.WriteLine(Day03.SolveP1());
-            Console.WriteLine(Day03.SolveP2());
-            Console.WriteLine(Day04.SolveP1());
-            Console.WriteLine(Day04.SolveP2());
-            Console.WriteLine(Day05.SolveP1());
-            Console.WriteLine(Day05.SolveP2());
+            var start = DateTime.Now;
+            Console.WriteLine("Day   Expected   {0}{1}", "Output".PadRight(20), "Duration");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" 1.a  1752       {0}{1}", Day01.SolveP1().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 1.b  1781       {0}{1}", Day01.SolveP2().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 2.a  1427868    {0}{1}", Day02.SolveP1().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 2.b  1568138742 {0}{1}", Day02.SolveP2().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 3.a  1307354    {0}{1}", Day03.SolveP1().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 3.b  482500     {0}{1}", Day03.SolveP2().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 4.a  45031      {0}{1}", Day04.SolveP1().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 4.b  2568       {0}{1}", Day04.SolveP2().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 5.a  7142       {0}{1}", Day05.SolveP1().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine(" 5.b  20012      {0}{1}", Day05.SolveP2().ToString().PadRight(20), (DateTime.Now - start).Milliseconds);
+            Console.WriteLine("--------------------------------------------------");
         }
     }
 }
